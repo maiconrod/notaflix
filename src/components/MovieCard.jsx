ee5b6acb7707d8daa0f51e27db44450e6b476a7b
Maicon Rodrigues <maicon_passos@hotmail.com>
2024-04-21 21:43:01 -0300

    Correção de pequenos bugs de imagem

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,11 +10,11 @@ const MovieCard = ({ movie, showLink = true }) => {
   return (
     <div
     style={{ backdropFilter: "blur(3px) opacity(100%)" }}
-    className="p-2 pb-4 mx-3 grid rounded-lg bg-gradient-to-t border border-gray-500">
+    className="p-2 pb-4 mx-3 grid justify-items-center items-center rounded-lg bg-gradient-to-t border border-gray-500 mb-3 min-w-72 max-w-72 sm:max-w-80">
         <img
           src={imageUrl + movie.poster_path}
           alt={movie.title}
-          className="mb-3 m-2 max-w-60 sm:max-w-48 md:max-w-48 lg:max-w-64 rounded-lg"
+          className="m-2 max-w-60 sm:max-w-48 md:max-w-48 lg:max-w-64 rounded-lg"
         />
         <h2 className="text-center">{movie.title}</h2>
         <p className="flex items-center gap-2 p-3 justify-center">
